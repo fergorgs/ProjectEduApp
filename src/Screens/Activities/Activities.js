8cfd04ae2d76c593850c4fc1ca32fae68082d854
alexandre jr <alexgalocha.jr@gmail.com>
2021-03-13 19:47:48 -0300

    connecting topics to server

--- src/Screens/Activities/Activities.js
+++ src/Screens/Activities/Activities.js
@@ -1,79 +1,85 @@
-import React, { useState, useEffect } from 'react'
-import { View, Text, ScrollView } from 'react-native'
-import { Card, Button,Header} from 'react-native-elements'
-   
+import React, { useState, useEffect } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { Card, Button, Header } from 'react-native-elements';
+
 import axios from 'axios';
 
 function Activities(props) {
-    const [ modules, setModules ] = useState(undefined);
+    const [modules, setModules] = useState(undefined);
 
     useEffect(() => {
         const fetch = async () => {
-            axios.get('http://192.168.15.2:8000/module')
-            .then((res) => {
-                setModules(res.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+            axios
+                .get('http://192.168.0.35:8000/module')
+                .then((res) => {
+                    setModules(res.data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         };
 
         fetch();
-    }, [ setModules, axios ]);
-    
-    const cards = modules?.length ? 
-            modules.map((mod, index) =>
-                <Card
-                    key={ mod.id }
-                    image={{ uri: mod.image ? mod.image : 'a' }}
-                    imageProps={{ 
-                        placeholderStyle: {
-                            backgroundColor: '#03A9F4'
-                        }
-                    }}
-                    title={ mod.name }
-                >   
-                    { /*
+    }, [setModules, axios]);
+
+    const cards = modules?.length
+        ? modules.map((mod, index) => (
+              <Card
+                  key={mod.id}
+                  image={{ uri: mod.image ? mod.image : 'a' }}
+                  imageProps={{
+                      placeholderStyle: {
+                          backgroundColor: '#03A9F4',
+                      },
+                  }}
+                  title={mod.name}
+              >
+                  {/*
                     <Text style={{marginBottom: 10, textAlign: 'center'}}>
                         Generic Module Title
                     </Text>
-                    */ }
-                    <Button
-                        type="solid"
-                        onPress = {() => props.navigation.navigate("Generic_Module", { mod: mod.id })}
-                        backgroundColor='#03A9F4'
-                        buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-                        title='View now' 
-                    />
-                </Card>
-            )
-            : null;
+                    */}
+                  <Button
+                      type="solid"
+                      onPress={() =>
+                          props.navigation.navigate('Generic_Module', {
+                              mod: mod.id,
+                          })
+                      }
+                      backgroundColor="#03A9F4"
+                      buttonStyle={{
+                          borderRadius: 0,
+                          marginLeft: 0,
+                          marginRight: 0,
+                          marginBottom: 0,
+                      }}
+                      title="View now"
+                  />
+              </Card>
+          ))
+        : null;
 
     return (
         <View>
             <Header
-                backgroundColor = '#1e272e'
+                backgroundColor="#1e272e"
                 leftComponent={{
                     icon: 'menu',
                     color: '#fff',
                     onPress: () => props.navigation.openDrawer(),
                 }}
-                centerComponent={{ 
-                    text: 'ACTIVITIES', 
-                    style: { color: '#fff' } 
+                centerComponent={{
+                    text: 'ACTIVITIES',
+                    style: { color: '#fff' },
                 }}
-                rightComponent={{ 
-                    icon: 'portrait', 
+                rightComponent={{
+                    icon: 'portrait',
                     color: '#fff',
-                    onPress: () => props.navigation.navigate("Perfil")
+                    onPress: () => props.navigation.navigate('Perfil'),
                 }}
-            /> 
-            <ScrollView
-                style={{ marginBottom: 80 }}
-            >
-                { cards }
-            </ScrollView>
+            />
+            <ScrollView style={{ marginBottom: 80 }}>{cards}</ScrollView>
         </View>
-    )
+    );
 }
 export default Activities;
--- src/Screens/Activities/Modules/SubModulesList.js
+++ src/Screens/Activities/Modules/SubModulesList.js
@@ -1,175 +1,206 @@
-import React, { Component } from 'react';
-import {StyleSheet,Text,ScrollView, Image,FlatList,TouchableOpacity,Dimensions,View} from 'react-native';
-import {Header} from 'react-native-elements'
-import ModuleIcon from './ModuleIcon.js'
-import {data} from './newData.js'
+import React, { useState, useEffect } from 'react';
+import {
+    StyleSheet,
+    Text,
+    ScrollView,
+    Image,
+    FlatList,
+    TouchableOpacity,
+    Dimensions,
+    View,
+} from 'react-native';
+import { Header } from 'react-native-elements';
+import ModuleIcon from './ModuleIcon.js';
+import { data } from './newData.js';
 
-export default class SubModulesList extends Component {
+import axios from 'axios';
 
-  constructor(props) {
-        super(props);
-        this.state = {
+function SubModulesList({ navigation }) {
+    const [topics, setTopics] = useState(undefined);
+    const { mod } = navigation.state.params;
 
-          mainData: data,
+    useEffect(() => {
+        const fetch = async () => {
+            axios
+                .get(`http://192.168.0.35:8000/topic/${mod}`)
+                .then((res) => {
+                    setTopics(res.data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         };
-      }
 
-  componentDidMount(){
+        fetch();
+    }, [setTopics, axios, mod]);
+    //constructor(props) {
+    //      super(props);
+    //      this.state = {
 
-    this.setState({subModules: this.state.mainData.map((subModule, index) => {
-    
-      // Pra cada submódulo, é criado um objeto
-      //esse vetor é depois reinderizado pela FlatList mais abaixo
-      return {
-        id:index,
-        name:subModule.subModuleName,
-        topics:subModule.subModuleTopics,
-        exercises:subModule.subModuleExercises,
-        image:subModule.subModuleImage
-        //navegation:"subModules"
-      }
-    })})
-  }
-  
-  //vai para a tela com a lista de tópicos, passando as informações
-  //do submódulo selecionado
-  clickEventListener = (item) => {
-    this.props.navigation.navigate('Topics', {
-      subModuleName:item.name,
-      topics:item.topics,
-      exercises:item.exercises
-    });
-  }
+    //        mainData: data,
+    //      };
+    //    }
 
-    
+    //componentDidMount(){
 
-  render() {
-    console.log(this.props.navigation.state.params)
+    //  this.setState({subModules: this.state.mainData.map((subModule, index) => {
+    //
+    //    // Pra cada submódulo, é criado um objeto
+    //    //esse vetor é depois reinderizado pela FlatList mais abaixo
+    //    return {
+    //      id:index,
+    //      name:subModule.subModuleName,
+    //      topics:subModule.subModuleTopics,
+    //      exercises:subModule.subModuleExercises,
+    //      image:subModule.subModuleImage
+    //      //navegation:"subModules"
+    //    }
+    //  })})
+    //}
+
+    //vai para a tela com a lista de tópicos, passando as informações
+    //do submódulo selecionado
+    const clickEventListener = (item) => {
+        navigation.navigate('Topics', {
+            subModuleName: item.name,
+            topics: item.topics,
+            exercises: item.exercises,
+        });
+    };
+
+    //render() {
+    //  console.log(this.props.navigation.state.params)
     return (
-      <View>
-        {/*Screen Header Information */}
-        <Header
-            backgroundColor = '#1e272e'
-            barStyle = 'light-content'
-            leftComponent={{
-            icon: 'arrow-back',
-            color: '#fff',
-            onPress: () => this.props.navigation.navigate("Activities"),
-            }}
-            centerComponent={{ text: 'Test Module', style: { color: '#fff' } }}
-        />
-        {/*Renders Topics with a FlatList */}
-        <FlatList 
-          //style={styles.contentList}
-          ListFooterComponent={<View style={{ height: 20 }}></View>}
-          columnWrapperStyle={styles.listContainer}
-          data={this.state.subModules}
-          keyExtractor= {(item) => {
-            return (item.id).toString();
-          }}
-          renderItem={({item}) => {
-          return (
-            <ModuleIcon item={item} ClickEventListener={this.clickEventListener}/>
-          )}}/>
-
-      </View>
+        <View>
+            {/*Screen Header Information */}
+            <Header
+                backgroundColor="#1e272e"
+                barStyle="light-content"
+                leftComponent={{
+                    icon: 'arrow-back',
+                    color: '#fff',
+                    onPress: () => navigation.navigate('Activities'),
+                }}
+                centerComponent={{
+                    text: 'Test Module',
+                    style: { color: '#fff' },
+                }}
+            />
+            {/*Renders Topics with a FlatList */}
+            <FlatList
+                //style={styles.contentList}
+                ListFooterComponent={<View style={{ height: 20 }}></View>}
+                columnWrapperStyle={styles.listContainer}
+                data={topics}
+                keyExtractor={(item) => {
+                    return item.id;
+                }}
+                renderItem={({ item }) => {
+                    return (
+                        <ModuleIcon
+                            item={item}
+                            ClickEventListener={clickEventListener}
+                        />
+                    );
+                }}
+            />
+        </View>
     );
-  }
+    //}
 }
 
+export default SubModulesList;
+
 const styles = StyleSheet.create({
-  logoImage:{
-    width: 200,
-    height: 200
-  },
-  containerLogo:{
-    alignItems:"center",
-    margin: 10,
-  },
-  container:{
-    flex:1,
-    backgroundColor:"#636e72"
-  },
-  contentList:{
-    flex:1,
-    paddingBottom:20
-  },
-  cardContent: {
-    marginLeft:20,
-    marginTop:10
-  },
-  image:{
-    width:90,
-    height:90,
-    borderRadius:45,
-    borderWidth:2,
-    borderColor:"#ebf0f7"
-  },
-  imagePlay:{
-    width:60,
-    height:60,
-  },
-  card:{
-    shadowColor: '#00000021',
-    shadowOffset: {
-      width: 0,
-      height: 6,
+    logoImage: {
+        width: 200,
+        height: 200,
     },
-    shadowOpacity: 0.37,
-    shadowRadius: 7.49,
-    elevation: 12,
+    containerLogo: {
+        alignItems: 'center',
+        margin: 10,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#636e72',
+    },
+    contentList: {
+        flex: 1,
+        paddingBottom: 20,
+    },
+    cardContent: {
+        marginLeft: 20,
+        marginTop: 10,
+    },
+    image: {
+        width: 90,
+        height: 90,
+        borderRadius: 45,
+        borderWidth: 2,
+        borderColor: '#ebf0f7',
+    },
+    imagePlay: {
+        width: 60,
+        height: 60,
+    },
+    card: {
+        shadowColor: '#00000021',
+        shadowOffset: {
+            width: 0,
+            height: 6,
+        },
+        shadowOpacity: 0.37,
+        shadowRadius: 7.49,
+        elevation: 12,
 
-    marginLeft: 20,
-    marginRight: 20,
-    marginTop:20,
-    backgroundColor:"white",
-    padding: 10,
-    flexDirection:'row',
-    borderRadius:30,
-  },
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 20,
+        backgroundColor: 'white',
+        padding: 10,
+        flexDirection: 'row',
+        borderRadius: 30,
+    },
 
-  name:{
-    fontSize:11,
-    flex:1,
-    alignSelf:'center',
-    color:"#000000",
-    fontWeight:'bold'
-  },
-  count:{
-    fontSize:14,
-    flex:1,
-    alignSelf:'center',
-    color:"#6666ff"
-  },
-  followButton: {
-    marginTop:10,
-    height:35,
-    width:100,
-    padding:10,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius:30,
-    backgroundColor: "white",
-    borderWidth:1,
-    borderColor:"#dcdcdc",
-  },
-  followButtonPlay: {
-    marginTop:10,
-    marginBottom: 10,
-    height:35,
-    width:100,
-    padding:10,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: "white",
-  },
-  followButtonText:{
-    color: "#dcdcdc",
-    fontSize:12,
-  },
-
-});  
-
-  
+    name: {
+        fontSize: 11,
+        flex: 1,
+        alignSelf: 'center',
+        color: '#000000',
+        fontWeight: 'bold',
+    },
+    count: {
+        fontSize: 14,
+        flex: 1,
+        alignSelf: 'center',
+        color: '#6666ff',
+    },
+    followButton: {
+        marginTop: 10,
+        height: 35,
+        width: 100,
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 30,
+        backgroundColor: 'white',
+        borderWidth: 1,
+        borderColor: '#dcdcdc',
+    },
+    followButtonPlay: {
+        marginTop: 10,
+        marginBottom: 10,
+        height: 35,
+        width: 100,
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    followButtonText: {
+        color: '#dcdcdc',
+        fontSize: 12,
+    },
+});
